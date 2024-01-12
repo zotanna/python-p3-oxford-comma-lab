14b877a35ede0b5c95a2c9170e2128787ef134e5
denis <denniiskim@gmail.com>
2024-01-12 04:15:52 +0300

    completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    pass
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        items[-1] = "and " + items[-1]
+        return ", ".join(items)
